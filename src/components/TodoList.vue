398ff335b85804ead83ab725d16752359a9740e2
bel3atar <bel3atar@aol.com>
2024-12-02 11:32:19 +0100

    cleanup

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,7 @@ const DISPLAY_FIRST = 10
 import { ref, useTemplateRef, onBeforeMount, computed, onMounted } from 'vue'
 import { getTodos } from '@/apis/todos'
 import type { Todo as TodoType } from '@/apis/todos'
-import { middleY } from '@/helpers/dom'
+import { findDropPosition } from '@/helpers/dom'
 import Todo from './Todo.vue'
 
 const id = ref(999) // id counter for user created todos
@@ -28,33 +28,16 @@ const handleDragStart = (ev) => {
   ev.dataTransfer.dropEffect = ev.dataTransfer.effectAllowed = 'move'
   draggedTodo.value = ev.target
 }
-const handleDragEnd = (ev, eventId) => (draggedTodo.value = null)
-const handleDrop = ({ pageY }) => {
-  let targetIdx = null
-  const children = elements.value.children
-  const lastIdx = children.length - 1
-  for (let i = 0; i <= children.length; ++i) {
-    const isFirst = i === 0
-    const isLast = i === children.length
-    const start = isFirst
-      ? -Infinity
-      : isLast
-        ? middleY(children[lastIdx])
-        : middleY(children[i - 1])
-    const end = isFirst ? middleY(children[0]) : isLast ? +Infinity : middleY(children[i])
-    if (pageY >= start && pageY < end) {
-      targetIdx = i
-      break
-    }
-  }
 
+const handleDrop = ({ pageY }) => {
+  const targetIdx = findDropPosition({ elementsList: elements.value.children, mouseY: pageY })
   const draggedId = +draggedTodo.value.getAttribute('data-id')
   const draggedIdx = todos.value.findIndex(({ id }) => id === draggedId)
-
   const isAscending = targetIdx < draggedIdx
+  const offset = isAscending ? 0 : -1 // if target is below initial, account for 1 delete before insert
 
   const [removedItem] = todos.value.splice(draggedIdx, 1)
-  todos.value.splice(targetIdx - (isAscending ? 0 : 1), 0, removedItem)
+  todos.value.splice(targetIdx + offset, 0, removedItem)
 }
 
 const addTodo = (title) =>
@@ -76,7 +59,7 @@ const addTodo = (title) =>
         v-bind="todo"
         :key="todo.id"
         @dragstart="handleDragStart"
-        @dragend="handleDragEnd($event, todo.id)"
+        @dragend="draggedTodo = null"
       />
     </ul>
     <input
--- src/helpers/dom.ts
+++ src/helpers/dom.ts
@@ -2,3 +2,31 @@ export const middleY = (el: HTMLElement): number => {
   const { y, height } = el.getBoundingClientRect()
   return y + height / 2
 }
+
+export const findDropPosition = ({
+  elementsList,
+  mouseY,
+}: {
+  elementsList: HTMLElement[]
+  mouseY: number
+}) => {
+  const lastIdx = elementsList.length - 1
+  // determine the range within which the mouse has dropped
+  // for a list with n elements there are n+1 possible drop positions
+  for (let i = 0; i <= elementsList.length; ++i) {
+    const isFirst = i === 0
+    const isLast = i === elementsList.length
+    const rangeStart = isFirst
+      ? -Infinity
+      : isLast
+        ? middleY(elementsList[lastIdx])
+        : middleY(elementsList[i - 1])
+    const rangeEnd = isFirst
+      ? middleY(elementsList[0])
+      : isLast
+        ? +Infinity
+        : middleY(elementsList[i])
+    const isMouseInRange = mouseY >= rangeStart && mouseY < rangeEnd
+    if (isMouseInRange) return i
+  }
+}
